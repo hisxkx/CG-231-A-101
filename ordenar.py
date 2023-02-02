92bc69b474bc527484b6333699bf112045b455e1
hisxkx <124268971+hisxkx@users.noreply.github.com>
2023-02-02 08:53:31 -0500

    Create ordenar.py

---
+++ ordenar.py
@@ -0,0 +1,8 @@
+num1 = int(input("Ingresa el primer número: ")) 
+num2 = int(input("Ingresa el segundo número: "))
+num3 = int(input("Ingresa el tercer número: "))
+num4 = int(input("Ingresa el cuarto número: "))
+list = [num1, num2, num3, num4]
+print (list)
+list.sort()
+print (list)
